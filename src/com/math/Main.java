ec3ffd8ecaeca5bc5ec035572f07be7973597aaa
geniusserg <shyundaid@gmail.com>
2020-03-02 12:26:30 +0300

    fix

--- src/com/math/Main.java
+++ src/com/math/Main.java
@@ -4,17 +4,12 @@ public class Main{
     public static void main(String args[]){
         System.out.println("math package");
         Complex Cd = new Complex(1, 2);
-        DoubleMatrix DMa = new DoubleMatrix(new double[][]{{1,1}, {1,1}});
-        DoubleMatrix DMb = new DoubleMatrix(new double[][]{{1}, {1}});
-        ComplexMatrix CMc = new ComplexMatrix(new Complex[][]{{Cd,Cd}, {Cd,Cd}});
+        Matrix CMa = new Matrix(new Complex[][]{{Cd,Cd}, {Cd,Cd}});
+        Matrix CMb = new Matrix(new Complex[][]{{Cd}, {Cd}});
         try{
-            DoubleMatrix DMr = DMa.add(DMa);
-            System.out.println(DMr);
-            ComplexMatrix CMp = CMc.minus(CMc);
+            Matrix CMp = CMa.minus(CMb);
 	        System.out.println(CMp);
-	        DoubleMatrix DMe = DMa.add(DMb);
-	        System.out.println(DMe);
-	}
+	    }
         catch(Exception ex){
             for (int i = 0; i < ex.getStackTrace().length; i++){
                 if (ex.getStackTrace()[i].getFileName().equals("Main.java")){
