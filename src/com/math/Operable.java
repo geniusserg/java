770e3dcaaa2fa74109f77bf6ee64c078d0030a87
geniusserg <50704370+geniusserg@users.noreply.github.com>
2020-03-02 11:51:58 +0300

    Delete Operable.java

--- src/com/math/Operable.java
+++
@@ -1,9 +0,0 @@
-package com.math;
-
-public interface Operable{
-    public Operable add(Operable B);
-    public Operable minus(Operable B);
-    public Operable multiply(Operable B);
-    public Operable divide(Operable B);
-    public String show();
-}
